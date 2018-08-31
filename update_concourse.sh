b0f0a3ffb8d2eaa7d96e38cfcc66ca8131c49a32
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2018-08-30 17:45:23 -0700

    udpated concourse

--- update_concourse.sh
+++ update_concourse.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # catch-microservice update_concourse.sh
 
-fly -t ci set-pipeline -p catch-microservice -c ci/pipeline.yml --load-vars-from ci/.credentials.yml
+fly -t ci set-pipeline -p catch-microservice -c ci/pipeline.yml --load-vars-from ../.credentials.yml
